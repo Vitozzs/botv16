205785cd1dd9b4629ca84699b2c887ff4e0de2cf
Vitozzs <97907295+Vitozzs@users.noreply.github.com>
2022-04-05 10:56:39 +0800

    Create session.js

---
+++ session.js
@@ -0,0 +1,11 @@
+{
+
+	"clientID": "EP2yoPOiNsR4f1rFu3Kqgg==",	"serverToken": "1@w8B35i1NRiqXsBJomF/PsH13+pMyOulYOnagThOxqlfKG/xCvo5TBloYEN7q7SqBPFtO9nutz6vclA==",
+
+	"clientToken": "SwW/TzewyKsB7ab8lF0iwXPLF0ZeUV35EhpW5nNr758=",
+
+	"encKey": "Q86RsT1zmbsW6nnu7IL9LdbWJ5CHApKyQcLv3RBriao=",
+
+	"macKey": "grfVW61+YsHUxckFPISTJh+eN36rhZ1rPV6nvK0bHak="
+
+}
